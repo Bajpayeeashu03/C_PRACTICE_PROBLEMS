68014cd8e8f6c50e169fa6d118775b9b4e775a04
ashutosh bajpayee <ashutoshbajpayee2020@gmail.com>
2023-01-02 20:53:05 +0530

    odd even code added

---
+++ FOR LOOP/odd_even_check.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int b;
+    printf("Enter the number : \n");
+    scanf("%d",&b);
+    if(b%2==0)
+    printf("The number is even ");
+    }
+    else
+    {
+    printf("The number is Odd ");
+    }
+    return 0;
+}
