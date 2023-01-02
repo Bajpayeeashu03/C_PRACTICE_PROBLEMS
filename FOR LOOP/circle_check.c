c68bc27af1871f5bd86de4cbb076b6f75f680367
ashutosh bajpayee <ashutoshbajpayee2020@gmail.com>
2023-01-02 21:16:42 +0530

    added

---
+++ FOR LOOP/circle_check.c
@@ -0,0 +1,28 @@
+include<stdio.h>
+int main()
+{
+    int x1,x2,x3,y1,y2,y3;
+    printf("Enter the first x cordinate \n");
+    scanf("%d",&x1);
+    printf("Enter the first y cordinate \n");
+    scanf("%d",&y1);
+    printf("Enter the second x cordinate \n");
+    scanf("%d",&x2);
+    printf("Enter the second y cordinate \n");
+    scanf("%d",&y2);
+    printf("Enter the third x cordinate \n");
+    scanf("%d",&x3);
+    printf("Enter the third y cordinate \n");
+    scanf("%d",&y3);
+    int s1 = ((y1-y2)/(x1-x2));
+    int s2 = ((y2-y3)/(x2-x3));
+    int s3 = ((y3-y1)/(x3-x1));
+    if(s1==s2==s3)
+    {
+        printf("They fall under same line");
+    }
+    else{
+        printf("They do not fall under same line");
+    }
+    return 0;
+
