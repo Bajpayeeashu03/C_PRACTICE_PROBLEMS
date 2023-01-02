bc4be6876302f6d9ee9026574bec1d40ad736df2
ashutosh bajpayee <ashutoshbajpayee2020@gmail.com>
2023-01-02 21:30:12 +0530

    age checker added

--- FOR LOOP/circle_check.c
+++ FOR LOOP/circle_check.c
@@ -1,28 +1,32 @@
-include<stdio.h>
+
+
+#include<stdio.h>
+#include<math.h>
 int main()
 {
-    int x1,x2,x3,y1,y2,y3;
-    printf("Enter the first x cordinate \n");
+    int x , y,r, x1,y1;
+    printf ("Enter the center x cordinate");
+    scanf("%d",&x);
+    printf ("Enter the center y cordinate");
+    scanf("%d",&y);
+    printf ("Enter the radius");
+    scanf("%d",&r);
+    printf ("Enter the point x cordinate");
     scanf("%d",&x1);
-    printf("Enter the first y cordinate \n");
+    printf ("Enter the point y cordinate");
     scanf("%d",&y1);
-    printf("Enter the second x cordinate \n");
-    scanf("%d",&x2);
-    printf("Enter the second y cordinate \n");
-    scanf("%d",&y2);
-    printf("Enter the third x cordinate \n");
-    scanf("%d",&x3);
-    printf("Enter the third y cordinate \n");
-    scanf("%d",&y3);
-    int s1 = ((y1-y2)/(x1-x2));
-    int s2 = ((y2-y3)/(x2-x3));
-    int s3 = ((y3-y1)/(x3-x1));
-    if(s1==s2==s3)
+    int d = sqrt(pow((x-x1),2)+pow((y-y1),2));
+    if(d<r)
     {
-        printf("They fall under same line");
+        printf("The point lies within the circle");
     }
-    else{
-        printf("They do not fall under same line");
+    else if(d>r)
+    {
+      printf("The point lies outside the circle");  
+    }
+    else(d==r){
+        printf("The point lies on the circle");
     }
     return 0;
-
+    
+}
