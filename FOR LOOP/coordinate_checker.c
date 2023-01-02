bce5b475d131df583fd066a7cd4469e6891d0135
ashutosh bajpayee <ashutoshbajpayee2020@gmail.com>
2023-01-02 21:27:23 +0530

    age checker added

---
+++ FOR LOOP/coordinate_checker.c
@@ -0,0 +1,23 @@
+#includstdio.>
+int main()
+{
+int x,y;
+printf("Enter The Value Of X");
+scanf("%d",&x);
+printf("Enter The Value Of Y");
+scanf("%d",&y);
+
+if(x==0)
+{printf("Point Lies On Y Axis");
+}
+else if (y==0)
+{printf("Point Lies On X Axis");
+}
+else if (x==0 && y==0)
+{printf{"point lies on origin");
+}
+else{
+printf("point does not lie on any axis neither the origin");
+}
+return 0;
+}
