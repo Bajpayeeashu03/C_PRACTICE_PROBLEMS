652c269ef70e007576b2117c2ee2842d35aa35e3
ashutosh bajpayee <ashutoshbajpayee2020@gmail.com>
2023-01-02 21:42:37 +0530

    age checker added

--- FOR LOOP/leap_year.c
+++ FOR LOOP/leap_year.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int a;
+    int y;
     printf("Enter the year \n");
-    scanf("%d",&a);
-    if(a%4==0)
+    scanf("%d",&y);
+    if(y%4==0)
     {
-    printf("the year %d is a leap year",a);
-
+    printf("year %d is a leap year",y);
     }
     else{
-    printf("the year %d is not a leap year",a);
+    printf("year %d is not a leap year",y);
     }
     return 0;
 }
