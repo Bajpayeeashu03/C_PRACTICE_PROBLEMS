ef4a749e5ba9c304d4307ef0c18588b1ce2a56f1
ashutosh bajpayee <ashutoshbajpayee2020@gmail.com>
2023-01-02 21:09:12 +0530

    added

---
+++ FOR LOOP/triangle_check.c
@@ -0,0 +1,18 @@
+include<stdio.h>
+int main()
+{ int a,b,c;
+    printf("Enter the first angle \n");
+    scanf("%d",&a);
+    printf("Enter the second angle \n");
+    scanf("%d",&b);
+    printf("Enter the third angle \n");
+    scanf("%d",&c);
+    if((a+b+c)==180)
+    {
+        printf("The triangle can be formed");
+    }
+    else{
+        printf("The triangle cannot be formed");
+    }
+    return 0;
+}
